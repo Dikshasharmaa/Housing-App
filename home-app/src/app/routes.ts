b2255d7251cbb991ed015e4ad97f471714addf59
Diksha Sharma <diksha66sharma78@gmail.com>
2025-05-23 00:44:22 -0400

    Added routing and retrived id for every housingLocation

--- home-app/src/app/routes.ts
+++ home-app/src/app/routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { DetailsComponent } from "./details/details.component";
 //Created a variable of type Routes assigned the value of empty array
-const routeConfig: Routes =[]; 
+const routeConfig: Routes =[
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details Page'
+    }
+]; 
 
 export default routeConfig;
