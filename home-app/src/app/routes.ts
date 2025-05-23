b67e6b0428faa3a1966f0425382ea632ae3027ff
Diksha Sharma <diksha66sharma78@gmail.com>
2025-05-23 00:06:15 -0400

    Created a routes.ys file for routing

---
+++ home-app/src/app/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+//Created a variable of type Routes assigned the value of empty array
+const routeConfig: Routes =[]; 
+
+export default routeConfig;
