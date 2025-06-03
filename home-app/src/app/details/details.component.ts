c6ded2b09c1bd26d2c1da806fa7043fb6f0bba8e
Diksha Sharma <diksha66sharma78@gmail.com>
2025-06-03 17:19:05 -0400

    Made changes in html elemts

--- home-app/src/app/details/details.component.ts
+++ home-app/src/app/details/details.component.ts
@@ -8,9 +8,15 @@ import { HousingLocation } from '../housing-location';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works!{{ housingLocation?.id}}
-    </p>
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo">
+      <section class ="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class ="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+        </section>
+    </article>    
+
+
   `,
   styleUrls: ['./details.component.css']
 })
